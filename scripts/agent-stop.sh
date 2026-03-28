cc9a70a7209d688e071b8d782b9493c87e62f3bf
Andrew Noblet <andrewbnoblet@gmail.com>
2026-03-28 06:21:26 -0400

    feat: simplify agent stop notification by removing reason input

--- scripts/agent-stop.sh
+++ scripts/agent-stop.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-INPUT=$(cat)
-REASON=$(echo "$INPUT" | jq -r '.reason')
 
-curl -d "Agent Stop: $REASON" ntfy.sh/acf0616f-8f2a-5461-88f2-363e53234446
+curl -d "Finished" ntfy.sh/acf0616f-8f2a-5461-88f2-363e53234446
